4042a153ee879d45bca6ac3c0eda484cf05da8bc
Shrusti <shrustiparmar1411@gmail.com>
2022-04-05 17:51:42 +0530

    final commit

--- script.ts
+++ script.ts
@@ -1,8 +1,8 @@
-let operationDiv:any = document.getElementById('operation-div');
-let resultDiv:any = document.getElementById('result-div');
+let operationDiv = document.getElementById('operation-div') as HTMLDivElement;
+let resultDiv = document.getElementById('result-div') as HTMLDivElement;
 
-function addChar(element) {
-    let char = element.getAttribute('data-value');
+function addChar(element: HTMLTableColElement): void {
+    let char: string = element.getAttribute('data-value')!;
     operationDiv.innerText += char;
 }
 
@@ -12,146 +12,142 @@ function clearScreen() {
 }
 
 //Backspace fun.
-function backspace() {
-    let operationValue = operationDiv.innerText;
-    let operationValueLength = operationValue.length;
-    let newOperationValue = operationValue.substring(0, operationValueLength-1);
+function backspace(): void {
+    let operationValue: string = operationDiv.innerText;
+    let operationValueLength: number = operationValue.length;
+    let newOperationValue: string = operationValue.substring(0, operationValueLength-1);
     operationDiv.innerText = newOperationValue;
 }
 
 //Calculate fun.
 function calculate() {
-    let operationValue = operationDiv.innerText;
+    let operationValue: string = operationDiv.innerText;
     operationDiv.innerText = eval(operationValue);
 }
 
 // trigonometry function to calculate sine,cos,tan,cot,cosec,sec
-function trigonometry(fun){
-    let operationValue = operationDiv.innerText;
-    let result;
+function trigonometry(fun: string){
+    let operationValue: number = +operationDiv.innerText;
+    let result: string|undefined;
     switch (fun) {
         case 'sine':
-            result = Math.sin(operationValue);
+            result = Math.sin(operationValue).toString();
             break;
         case 'cos':
-            result = Math.cos(operationValue);
+            result = Math.cos(operationValue).toString();
             break; 
         case 'tan':
-            result = Math.tan(operationValue);
+            result = Math.tan(operationValue).toString();
             break;
         case 'cot':
-            result = 1 / Math.tan(operationValue);
+            result = (1 / Math.tan(operationValue)).toString();
             break;  
         case 'cosec':
-            result = 1 / Math.sin(operationValue);
+            result = (1 / Math.sin(operationValue)).toString();
             break;
         case 'sec':
-            result = 1 / Math.cos(operationValue);
+            result = (1 / Math.cos(operationValue)).toString();
             break;         
     }
     if(result === 'NaN'){
         operationDiv.innerText = "Error";
     }else{
-        operationDiv.innerText = result;
+        operationDiv.innerText = result!;
     }
 }
 
 function calc_square(){
-    let operationValue = operationDiv.innerText;
-    operationDiv.innerText = operationValue * operationValue;
+    let operationValue = +operationDiv.innerText;
+    operationDiv.innerText = (operationValue * operationValue).toString();
 }
 
 function calc_absolute(){
-    operationDiv.innerText = Math.abs(operationDiv.innerText);
+    operationDiv.innerText = Math.abs(+operationDiv.innerText).toString();
 }
 
 function calc_exp(){
-    operationDiv.innerText = Math.exp(operationDiv.innerText);
+    operationDiv.innerText = Math.exp(+operationDiv.innerText).toString();
 }
 
 function calc_sqrt(){
-    operationDiv.innerText = Math.sqrt(operationDiv.innerText);
+    operationDiv.innerText = Math.sqrt(+operationDiv.innerText).toString();
 }
 
 function calc_factorial(){
-    let operationValue = operationDiv.innerText;
+    let operationValue = +operationDiv.innerText;
     let result = 1;
     for(let i = operationValue;i >= 1; i--){
         result *= i;
     }
-    operationDiv.innerText = result;
+    operationDiv.innerText = result.toString();
 }
 
 function calc_ten_pow(){
-    operationDiv.innerText = Math.pow(10,operationDiv.innerText);
+    operationDiv.innerText = Math.pow(10,+operationDiv.innerText).toString();
 }
 
 // log base 10 function
 function calc_log(){
-    operationDiv.innerText = Math.log10(operationDiv.innerText);
+    operationDiv.innerText = Math.log10(+operationDiv.innerText).toString();
 }
 
 //natural logarithm function
 function calc_ln(){
-    operationDiv.innerText = Math.log(operationDiv.innerText);
+    operationDiv.innerText = Math.log(+operationDiv.innerText).toString();
 }
 
 // MC, MR, M+, M-, MS funtionality
-let memoryArr = [];
-let mStatus = 0;
+let memoryArr: Array<number> = [];
+let mStatus: number = 0;
 
-var memoryRegister = [];
 function memoryClear(){
-    // memoryArr = [];
-    // if(mStatus == 1){
-    //     document.getElementById('memory-clear').style.opacity = 0.2;
-    // }
-    memoryRegister = [];
+    memoryArr = [];
+    if(mStatus == 1){
+        document.getElementById('memory-clear')!.style.opacity = "0.2";
+        document.getElementById('memory-recall')!.style.opacity = "0.2";
+    }
 }
 
 function memoryRecall(){
-    let sum = 0;
+    let sum: number = 0;
     for(let x of memoryArr){
-        sum += Number(x);
+        sum += x;
     }
-    operationDiv.innerText = sum;
+    operationDiv.innerText = sum.toString();
 }
 
 function memoryPlus(){
     if(operationDiv.innerText != ""){
-        memoryArr.push(operationDiv.innerText);
+        memoryArr.push(+operationDiv.innerText);
     }
     if(mStatus == 0 && operationDiv.innerText != ""){
-        // document.getElementById('memory-clear').style.opacity = 1;
-        // document.getElementById('memory-recall').style.opacity = 1;
-        // mStatus = 1;
-        memoryRegister.push();
+        document.getElementById('memory-clear')!.style.opacity = "1";
+        document.getElementById('memory-recall')!.style.opacity = "1";
+        mStatus = 1;
     }
     operationDiv.innerText = ""; 
 }
 
 function memoryMinus(){
     if(operationDiv.innerText != ""){
-        memoryArr.push('-'+operationDiv.innerText);
+        memoryArr.push(+operationDiv.innerText * -1);
     }
     if(mStatus == 0 && operationDiv.innerText != ""){
-        // document.getElementById('memory-clear').style.opacity = 1;
-        // document.getElementById('memory-recall').style.opacity = 1;
-        // mStatus = 1;
-        memoryRegister.pop();
+        document.getElementById('memory-clear')!.style.opacity = "1";
+        document.getElementById('memory-recall')!.style.opacity = "1";
+        mStatus = 1;
     }
     operationDiv.innerText = "";
 }
 
 function memorySave(){
     if(operationDiv.innerText != ""){
-        memoryArr.push(operationDiv.innerText);
+        memoryArr.push(+operationDiv.innerText);
     }
     if(mStatus == 0 && operationDiv.innerText != ""){
-        // document.getElementById('memory-clear').style.opacity = 1;
-        // document.getElementById('memory-recall').style.opacity = 1;
-        // mStatus = 1;
-        memoryRegister = [];
+        document.getElementById('memory-clear')!.style.opacity = "1";
+        document.getElementById('memory-recall')!.style.opacity = "1";
+        mStatus = 1;
     }
     operationDiv.innerText = "";
 }
